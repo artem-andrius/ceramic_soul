03d11a19f0f5ba7a76569715b6a09142cd659165
Artem <test@gmail.com>
2026-05-04 16:07:03 +0300

    sections works part 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,37 @@
+// Підключаємо свайпер
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// Імпорт стилів слайдера
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import '/src/sass/style.scss';
+
+try {
+  // Підключаємо механізм слайдера
+  new Swiper('.works__slider', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      1920: {
+        spaceBetween: 35,
+      },
+    },
+    modules: [Navigation, Pagination],
+  });
+} catch (e) {}
